cadd2722c262ec3f3c25c18e7f93f663a78d55b8
Harish3268 <harishpadatare100@gamil.com>
2025-05-07 15:05:47 +0530

    basic operation

---
+++ BasicOperation.java
@@ -0,0 +1,12 @@
+package Student;
+
+public class BasicOperation {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 5;
+
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+    }
+}
