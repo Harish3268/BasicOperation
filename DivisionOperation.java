3f3034ce657377435389b40a08458c287715dad7
Harish3268 <harishpadatare100@gmail.com>
2025-05-07 15:16:14 +0530

    master

---
+++ DivisionOperation.java
@@ -0,0 +1,16 @@
+package Student;
+
+public class DivisionOperation {
+    public static void main(String[] args){
+        int a = 15;
+        int b = 0;
+        try {
+            System.out.println("Division: " + (a / b));
+        } catch (ArithmeticException e) {
+            System.out.println("Error: Division by zero is not allowed.");
+        } finally {
+            System.out.println("This block always executes.");
+        }
+    }
+    
+}
